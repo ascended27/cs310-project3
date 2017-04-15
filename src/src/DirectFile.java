efb7d2dcbe5eda9ecffb1f1fd7422f89c2a7c781
ascended27 <ascended27@gmail.com>
2017-04-14 23:32:26 -0400

    Added Insert and Find record
    Can now read and write disk from Direct File. Added comments to DirectFile.java

--- src/src/DirectFile.java
+++ src/src/DirectFile.java
@@ -15,7 +15,7 @@ public class DirectFile {
 	private int firstOverflow; // sector number
 	private int overflowBuckets; // count of overflow buckets in use
 	private int sectorSize;
-	
+
 	public DirectFile(Disk disk, int recordSize, int keySize, int firstAllocated, int bucketsAllocated) {
 		this.disk = disk;
 		this.recordSize = recordSize;
@@ -32,96 +32,181 @@ public class DirectFile {
 	}
 
 	public boolean insertRecord(char[] record) {
+		// Get the bucket the record should be inserted in
 		int bucket = hash(getKey(record));
-		disk.readSector(bucket + firstAllocated, buffer);
 
+		// Copy the record to insert and attempt to find it. If it exists then
+		// we have a duplicate key error.
 		char[] tmp = new char[recordSize];
-		copyArr(record,tmp);
-		
-		if(findRecord(tmp))
+		copyArr(record, tmp);
+		if (findRecord(tmp))
 			return false;
-		
-		int index = getEmptyRecord();
-		if(index == sectorSize){
-			// Buffer overflowed
-		}
 
-		for(int i = 0; i < recordSize; i++){
+		// Retrieve the first empty record in the buffer. If there are none in
+		// this sector then the buffer will become the first sector with an open
+		// space.
+		int index = getEmptyRecord(bucket);
+
+		// Insert into the sector buffer
+		for (int i = 0; i < recordSize; i++) {
 			buffer[index++] = record[i];
 		}
-		disk.writeSector(bucket, buffer);
+
+		// Write the updated sector buffer to the disk.
+		disk.writeSector(bucket + overflowBuckets, buffer);
+
+		// Operation was successful so return true.
 		return true;
 	}
 
 	public boolean findRecord(char[] record) {
-		if(overflowBuckets != 0){
-			// there is a chance the record is in an overflow bucket
-		}
-		
+		// Get the bucket the record should be inserted in.
 		int bucket = hash(getKey(record));
-		System.out.println(bucket);
-		disk.readSector(bucket+firstAllocated, buffer);
-		int index=-1;
-		for(int i = 0; i < sectorSize; i *= recordSize){
-			if(checkKey(i, record)){
+
+		// Read the sector the record should be into the buffer.
+		disk.readSector(bucket, buffer);
+
+		// Search through the buffer for the key.
+		int index = -1;
+		for (int i = 0; i < sectorSize; i += recordSize) {
+			// If the key is in the buffer then set the index to the start of
+			// the record.
+			if (checkKey(i, record)) {
 				index = i;
 				break;
 			}
-			if(i == 0)
-				i = 1;
 		}
-		
-		if(index != -1){
-			for(int i = 0; i < recordSize; i++){
+
+		// If the record was found then copy it to the passed char[]
+		if (index != -1) {
+			for (int i = 0; i < recordSize; i++) {
 				record[i] = buffer[index++];
 			}
+			// Operation was successful so return true. The record was found.
 			return true;
 		}
-		
+		// Otherwise it was not in that sector so move to the next overflow
+		// bucket if there are any.
+		else {
+			// If there are any overflow buckets then we have to look there too.
+			if (overflowBuckets != 0) {
+				// Loop over the overflow buckets searching for the key.
+				int count = 1;
+				while (count <= overflowBuckets) {
+					disk.readSector(bucket + count, buffer);
+					for (int i = 0; i < sectorSize; i += recordSize) {
+						// If the key is in the buffer then set the index to the
+						// start of the record.
+						if (checkKey(i, record)) {
+							index = i;
+							break;
+						}
+					}
+					// If it is found then copy it to the passed char[].
+					if (index != -1) {
+						for (int i = 0; i < recordSize; i++) {
+							record[i] = buffer[index++];
+						}
+						// Operation successful so return true. The record was
+						// found.
+						return true;
+					}
+					count++;
+				}
+			}
+		}
+		// Operation has failed so return false. The record was not found.
 		return false;
 	}
 
 	private int hash(char[] key) {
-		return Math.abs(key.toString().hashCode()) % (bucketsAllocated);
+		// Convert the key to a string
+		String tmp = "";
+		for (char letter : key)
+			tmp += letter;
+		// Return the hash value of the key.
+		return Math.abs(tmp.hashCode()) % (bucketsAllocated - 1);
 	}
 
-	private boolean checkKey(int index,char[] key){
-		for(int i = 0; i < keySize; i++){
-			if(buffer[index+i] != key[i])
-				return false;
+	private boolean checkKey(int index, char[] key) {
+		// Retrieve the key at the passed index in the buffer.
+		String keyAtIndex = "";
+		for (int i = index; i < index + keySize; i++) {
+			if (buffer[i] != '#')
+				keyAtIndex += buffer[i];
+			else
+				break;
 		}
-		return true;
+
+		// Convert the char[] passed to a string
+		String keyToCheck = "";
+		for (int i = 0; i < keyAtIndex.length(); i++)
+			keyToCheck += key[i];
+
+		// Return if they are equal.
+		return keyAtIndex.equals(keyToCheck);
+
 	}
-	
+
 	// Copy arr1 to arr2
 	private boolean copyArr(char[] src, char[] dest) {
+		// If the src array is smaller than the destination then proceed to
+		// copy.
 		if (src.length <= dest.length) {
 			for (int i = 0; i < src.length; i++)
 				dest[i] = src[i];
+			// Operation was successful so return true.
 			return true;
 		}
+		// Operation failed so return false.
 		return false;
 	}
 
 	private char[] getKey(char[] record) {
+		// Initialize a new array to store the key.
 		char[] key = new char[keySize];
 
+		// Loop over the record and extract the key.
 		for (int i = 0; i < keySize; i++) {
 			if (record[i] == '#')
 				break;
 			key[i] = record[i];
 		}
 
+		// Return the key.
 		return key;
 	}
 
-	private int getEmptyRecord() {
+	private int getEmptyRecord(int bucket) {
+		// If there are overflow buckets and the last character in the current
+		// buffer isn't a null character then move the next sector to the buffer
+		// this one is full.
+		if (overflowBuckets != 0 && buffer[buffer.length - 1] != '\000') {
+			bucket += overflowBuckets++;
+			disk.readSector(bucket, buffer);
+		}
+		// If the current buffer is full and there are no overflow buckets then
+		// move the next sector on the disk to the buffer.
+		if (buffer[buffer.length - 1] != '\000') {
+			while (buffer[buffer.length - 1] != '\000') {
+				if (overflowBuckets == 0) {
+					firstOverflow = bucket;
+					overflowBuckets++;
+					bucket = overflowBuckets + bucket;
+				} else
+					bucket += overflowBuckets++;
+				disk.readSector(bucket, buffer);
+			}
+		}
+
+		// Retrieve an index of the first empty space in the sector buffer.
 		int i = 0;
-		while (i < sectorSize){
+		while (i < sectorSize) {
 			if (buffer[i] == '\000')
 				return i;
 			i++;
 		}
-		return ++i;
+		return i;
 	}
+
 }
--- src/src/Driver.java
+++ src/src/Driver.java
@@ -3,43 +3,99 @@ package src;
 public class Driver {
 
 	public static void main(String[] args) {
-		Disk disk = new Disk();
-		DirectFile df = new DirectFile(disk, 60, 27, 0, 2);
+		Disk disk = new Disk(2000, 120);
+		DirectFile df = new DirectFile(disk, 60, 27, 0, 600);
 
-		String testString1 = "A-1 Peak#United States#12377";
-		String testString2 = "Abbot-Petit Griffon, Mount#United States#13715";
+		String testString1 = "AaAaAa#United States#12377";
+		String testString2 = "AaAaBB#United States#13715";
+		String testString3 = "AaBBAa#United States#13612";
+		String testString4 = "AaBBBB#United States#12344";
+		String testString5 = "BBAaAa#United States#12344";
 
-		String key1 = "A-1 Peak";
-		String key2 = "Abbot-Petit Griffon, Mount";
+		String key1 = "AaAaAa";
+		String key2 = "AaAaBB";
+		String key3 = "AaBBAa";
+		String key4 = "AaBBBB";
+		String key5 = "BBAaAa";
 
 		char[] tmp1 = testString1.toCharArray();
 		char[] tmp2 = testString2.toCharArray();
 		char[] tmp3 = key1.toCharArray();
 		char[] tmp4 = key2.toCharArray();
-		
+		char[] tmp5 = testString3.toCharArray();
+		char[] tmp6 = key3.toCharArray();
+		char[] tmp7 = testString4.toCharArray();
+		char[] tmp8 = key4.toCharArray();
+		char[] tmp9 = testString5.toCharArray();
+		char[] tmp10 = key5.toCharArray();
+
 		char[] s1 = new char[60];
 		char[] s2 = new char[60];
+		char[] s3 = new char[60];
+		char[] s4 = new char[60];
+		char[] s5 = new char[60];
+
 		char[] k1 = new char[60];
 		char[] k2 = new char[60];
-		
-		for(int i = 0; i < tmp1.length; i++)
+		char[] k3 = new char[60];
+		char[] k4 = new char[60];
+		char[] k5 = new char[60];
+
+		for (int i = 0; i < tmp1.length; i++)
 			s1[i] = tmp1[i];
-		for(int i = 0; i < tmp2.length; i++)
+		for (int i = tmp1.length; i < 60; i++)
+			s1[i] = ' ';
+		for (int i = 0; i < tmp2.length; i++)
 			s2[i] = tmp2[i];
-		for(int i = 0; i < tmp3.length; i++)
+		for (int i = tmp2.length; i < 60; i++)
+			s2[i] = ' ';
+		for (int i = 0; i < tmp5.length; i++)
+			s3[i] = tmp5[i];
+		for (int i = tmp5.length; i < 60; i++)
+			s3[i] = ' ';
+		for (int i = 0; i < tmp7.length; i++)
+			s4[i] = tmp7[i];
+		for (int i = tmp7.length; i < 60; i++)
+			s4[i] = ' ';
+		for (int i = 0; i < tmp9.length; i++)
+			s5[i] = tmp9[i];
+		for (int i = tmp7.length; i < 60; i++)
+			s5[i] = ' ';
+		for (int i = 0; i < tmp3.length; i++)
 			k1[i] = tmp3[i];
-		for(int i = 0; i < tmp4.length; i++)
+		for (int i = 0; i < tmp4.length; i++)
 			k2[i] = tmp4[i];
-		
+		for (int i = 0; i < tmp6.length; i++)
+			k3[i] = tmp6[i];
+		for (int i = 0; i < tmp8.length; i++)
+			k4[i] = tmp8[i];
+		for (int i = 0; i < tmp10.length; i++)
+			k5[i] = tmp10[i];
+
 		System.out.println("Insert s1: " + df.insertRecord(s1));
-//		System.out.println("Insert s2: " + df.insertRecord(s2));
+		System.out.println("Insert s2: " + df.insertRecord(s2));
+		System.out.println("Insert s3: " + df.insertRecord(s3));
+		System.out.println("Insert s4: " + df.insertRecord(s4));
+		System.out.println("Insert s5: " + df.insertRecord(s5));
 		System.out.println("Find s1: " + df.findRecord(k1));
-//		System.out.println("Find s2: " + df.findRecord(k2));
+		System.out.println("Find s2: " + df.findRecord(k2));
+		System.out.println("Find s3: " + df.findRecord(k3));
+		System.out.println("Find s4: " + df.findRecord(k4));
+		System.out.println("Find s5: " + df.findRecord(k5));
 		
-		for(int i = 0; i < k1.length; i++)
+		for (int i = 0; i < k1.length; i++)
 			System.out.print(k1[i]);
 		System.out.println();
-		for(int i = 0; i < k2.length; i++)
+		for (int i = 0; i < k2.length; i++)
 			System.out.print(k2[i]);
+		System.out.println();
+		for (int i = 0; i < k3.length; i++)
+			System.out.print(k3[i]);
+		System.out.println();
+		for (int i = 0; i < k4.length; i++)
+			System.out.print(k4[i]);
+		System.out.println();
+		for (int i = 0; i < k5.length; i++)
+			System.out.print(k5[i]);
 	}
 }
