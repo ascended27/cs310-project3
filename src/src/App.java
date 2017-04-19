da63f883422c03c041bd596fa36dcc7fd03b5e57
ascended27 <ascended27@gmail.com>
2017-04-19 16:28:32 -0400

    Added App class
    Added the app class and updated Driver to use it.

---
+++ src/src/App.java
@@ -0,0 +1,92 @@
+package src;
+
+import java.util.Scanner;
+
+public class App {
+
+	private int keySize = 27;
+	private int locSize = 27;
+	private int altSize = 6;
+	private DirectFile df;
+	
+	public App(DirectFile df){
+		this.df = df;
+		//TODO: Load file
+	}
+	
+	public void start() {
+		boolean sentinel = true;
+		Scanner in = new Scanner(System.in);
+		// Menu loop
+		while (sentinel) {
+			printMenu();
+			int action = Integer.parseInt(in.nextLine());
+			String key;
+			switch (action) {
+			case 1:
+				System.out.printf("\nMountain Name: ");
+				key = in.nextLine();
+				if (key.length() > 27)
+					key = key.substring(0, 26);
+				System.out.printf("Country: ");
+				String loc = in.nextLine();
+				if (loc.length() > 27)
+					loc = loc.substring(0, 26);
+				System.out.printf("Altitude: ");
+				String alt = in.nextLine();
+				if (alt.length() > 6)
+					alt = alt.substring(0, 5);
+				String insert = format(key,loc,alt);
+				if (df.insertRecord(insert.toCharArray()))
+					System.out.printf("\nRecord inserted successfully\n\n");
+				else
+					System.out.printf("\nRecord insert failed\n\n");
+				break;
+			case 2:
+				System.out.printf("\nMountain Name: ");
+				key = in.nextLine();
+				if (key.length() > 27)
+					key = key.substring(0, 26);
+				char[] record = key.toCharArray();
+				if (df.findRecord(record)) {
+					for (int i = 0; i < record.length; i++) {
+						if (record[i] != '#')
+							System.out.print(record[i]);
+						else
+							System.out.print(',');
+					}
+					System.out.println();
+				} else
+					System.out.printf("\nRecord not found\n\n");
+				break;
+			case 3:
+				in.close();
+				sentinel = false;
+				break;
+			default:
+				System.out.printf("Please enter a valid option\n\n");
+			}
+
+		}
+	}
+
+	//TODO: Load file 
+	private void loadFile(String fileName){
+		
+	}
+	
+	private String format(String key, String loc, String alt){
+		for(int i = key.length(); i < keySize-1; i++)
+			key += " ";
+		for(int i = loc.length(); i < locSize-1; i++)
+			loc += " ";
+		for(int i = alt.length(); i < altSize; i++)
+			alt += " ";
+		
+		return key+"#"+loc+"#"+alt;
+	}
+	
+	private void printMenu() {
+		System.out.printf("1.Insert Record\n2.Find Record\n3.Quit\n-> ");
+	}
+}
--- src/src/Driver.java
+++ src/src/Driver.java
@@ -6,40 +6,7 @@ public class Driver {
 		Disk disk = new Disk();
 		DirectFile df = new DirectFile(disk, 60, 27, 0, 2);
 
-		String testString1 = "A-1 Peak#United States#12377";
-		String testString2 = "Abbot-Petit Griffon, Mount#United States#13715";
-
-		String key1 = "A-1 Peak";
-		String key2 = "Abbot-Petit Griffon, Mount";
-
-		char[] tmp1 = testString1.toCharArray();
-		char[] tmp2 = testString2.toCharArray();
-		char[] tmp3 = key1.toCharArray();
-		char[] tmp4 = key2.toCharArray();
-		
-		char[] s1 = new char[60];
-		char[] s2 = new char[60];
-		char[] k1 = new char[60];
-		char[] k2 = new char[60];
-		
-		for(int i = 0; i < tmp1.length; i++)
-			s1[i] = tmp1[i];
-		for(int i = 0; i < tmp2.length; i++)
-			s2[i] = tmp2[i];
-		for(int i = 0; i < tmp3.length; i++)
-			k1[i] = tmp3[i];
-		for(int i = 0; i < tmp4.length; i++)
-			k2[i] = tmp4[i];
-		
-		System.out.println("Insert s1: " + df.insertRecord(s1));
-//		System.out.println("Insert s2: " + df.insertRecord(s2));
-		System.out.println("Find s1: " + df.findRecord(k1));
-//		System.out.println("Find s2: " + df.findRecord(k2));
-		
-		for(int i = 0; i < k1.length; i++)
-			System.out.print(k1[i]);
-		System.out.println();
-		for(int i = 0; i < k2.length; i++)
-			System.out.print(k2[i]);
+		App app = new App(df);
+		app.start();
 	}
 }
