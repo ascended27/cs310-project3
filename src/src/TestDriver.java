25f325b9deda39aca43c28b5746ce40f24130dc6
ascended27 <ascended27@gmail.com>
2017-04-27 00:15:48 -0400

    Test Class Added
    Can now test record insertions

--- src/src/TestDriver.java
+++ src/src/TestDriver.java
@@ -5,9 +5,10 @@ import static org.junit.Assert.assertEquals;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
+import org.junit.After;
 import org.junit.Test;
-
 
 public class TestDriver {
 
@@ -15,16 +16,17 @@ public class TestDriver {
 	Disk disk = new Disk(2000, 512);
 	DirectFile df = new DirectFile(disk, 60, 27, 1024, 600);
 	App app = new App(df);
-	
+	ArrayList<String> failures = new ArrayList<String>();
+
 	@Test
 	public void test(){
-		app.loadFile("mountains.txt");
 		
+		app.loadFile("mountains.txt");
 		try {
 			String path = new File("").getAbsolutePath() +"/src/src/mountains.txt";
 			path = path.replace("\\", "/");
 			BufferedReader br = new BufferedReader(new FileReader(path));
-
+			boolean success = false;
 			String line = br.readLine();
 			while (line != null) {
 				String key = line.substring(0, line.indexOf("#"));
@@ -33,10 +35,17 @@ public class TestDriver {
 				line = line.substring(line.indexOf("#") + 1, line.length());
 				String alt = line;
 
-				assertEquals(df.insertRecord(format(key,loc,alt).toCharArray()),true);
+				
+				success = df.findRecord(format(key,loc,alt).toCharArray());
+				assertEquals(true,success);
+				
+				if(!success)
+					failures.add(key);
+				
 				line = br.readLine();
 			}
-
+			for(String key : failures)
+				System.out.println(key);
 			br.close();
 			// If an error occurs notify that user.
 		} catch (Exception e) {
@@ -55,5 +64,13 @@ public class TestDriver {
 		return key.substring(0,27) + loc.substring(0, 27) + alt;
 	}
 	
+	@After
+	public void printResults(){
+		if(!failures.isEmpty())
+			for(String key : failures)
+				System.out.println(key);
+		else
+			System.out.println("All records found");
+	}
 	
 }
